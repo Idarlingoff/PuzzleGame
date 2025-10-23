be6c345b0da10f8b342c7358970e890fdaaacd23
bramandl <idarling.off@gmail.com>
2025-10-23 15:51:23 +0200

    New class created : - Player - Plate with children - Display
    Modified class :
    - ColorEnum (add color)
    - ShapeEnum (add shape)
    - PuzzleGame

---
+++ src/core/plates/PressurePlate.ts
@@ -0,0 +1,34 @@
+import { Plate } from "./Plate.js";
+import { Color } from "../enum/ColorEnum.js";
+import { Shape } from "../enum/ShapeEnum.js";
+
+export class PressurePlate extends Plate {
+
+    private _linkedDoorId?: string;
+
+    constructor(x: number, y: number, color: Color, linkedDoorId?: string) {
+        super(x, y, color, Shape.SQUARE);
+        this._linkedDoorId = linkedDoorId;
+    }
+
+    override onPlayerEnter(): void {
+        super.onPlayerEnter();
+    }
+
+    override onPlayerLeave(): void {
+        super.onPlayerLeave();
+    }
+
+    // ### GETTER - SETTER ### //
+    get kind(): "golden" | "pressure" {
+        return "pressure";
+    }
+
+    get linkedDoorId(): string | undefined {
+        return this._linkedDoorId;
+    }
+
+    set linkedDoorId(value: string | undefined) {
+        this._linkedDoorId = value;
+    }
+}
