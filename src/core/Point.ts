d744f625a035be2621a0904dc72858439ace2a6b
bramandl <idarling.off@gmail.com>
2025-10-23 14:16:35 +0200

    New class created : - Point - ColorEnum - ShapeEnum

---
+++ src/core/Point.ts
@@ -0,0 +1,49 @@
+import {Color} from "./enum/ColorEnum";
+import {Shape} from "./enum/ShapeEnum";
+
+export class Point {
+
+    private _coordonneesX : number;
+    private _coordonneesY : number;
+    private _color : Color;
+    private _shape : Shape;
+
+    constructor(x: number, y: number, color: Color, shape: Shape) {
+        this._coordonneesX = x;
+        this._coordonneesY = y;
+        this._color = color;
+        this._shape = shape;
+    }
+
+    get coordonneesX(): number {
+        return this._coordonneesX;
+    }
+
+    set coordonneesX(value: number) {
+        this._coordonneesX = value;
+    }
+
+    get coordonneesY(): number {
+        return this._coordonneesY;
+    }
+
+    set coordonneesY(value: number) {
+        this._coordonneesY = value;
+    }
+
+    get color(): Color {
+        return this._color;
+    }
+
+    set color(value: Color) {
+        this._color = value;
+    }
+
+    get shape(): Shape {
+        return this._shape;
+    }
+
+    set shape(value: Shape) {
+        this._shape = value;
+    }
+}
---
+++ src/core/enum/ShapeEnum.ts
@@ -0,0 +1,6 @@
+export enum Shape {
+  CIRCLE = "circle",
+  SQUARE = "square",
+  TRIANGLE = "triangle",
+  STAR = "star",
+}
